3471ace9868c1ea5ac7f5bb6636a9f2f47eb3e77
SmallZhezhe <liyunzhe@whu.edu.cn>
2019-10-31 09:27:48 +0800

    git test

--- n_array.cpp
+++ n_array.cpp
@@ -9,6 +9,7 @@
   但是，由于某种原因，最后会多一些没有意义的零*/
 
 
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -23,10 +24,10 @@ int num = 0;
 void get_result_in_vector(vector<vector<int> > & vec,int N,vector<int> &tmp, vector<vector<int>>& tmp_result)
 {
     for(int i = 0;i<vec[N].size();++i)
-    {        
+    {
         if(find(tmp.begin(),tmp.end(),vec[N][i])!=tmp.end())
         {
-            ;       
+            ;
         }
         else{
         tmp.push_back(vec[N][i]);
@@ -52,13 +53,13 @@ void get_all_combination(vector<vector<int>>& vec, int result[][nn])
 {
     vector<int> tmp_vec;
 
-    vector<vector<int>> tmp_result; 
+    vector<vector<int>> tmp_result;
     get_result_in_vector(vec,0,tmp_vec, tmp_result);
 
     //将vector中的数据转化为数组保存
     for (int i = 0; i < tmp_result.size(); i++){
         for (int j = 0; j < tmp_result.at(i).size(); j++)
-        {         
+        {
             result[i][j] = tmp_result.at(i).at(j);
             num++;
         }
@@ -67,7 +68,7 @@ void get_all_combination(vector<vector<int>>& vec, int result[][nn])
 
 int main()
 {
-        
+
     int arr1[] = {2,4};
     int arr2[] = {3,5,6,7,8,9,11};
     int arr3[] = {1,10};
@@ -88,7 +89,7 @@ int main()
 
     start = clock();
 
-    vector<vector<int>> vec; 
+    vector<vector<int>> vec;
     vec.push_back(vector<int>(arr1,arr1+sizeof(arr1)/sizeof(arr1[0])));
     vec.push_back(vector<int>(arr2,arr2+sizeof(arr2)/sizeof(arr2[0])));
     vec.push_back(vector<int>(arr3,arr3+sizeof(arr3)/sizeof(arr3[0])));
